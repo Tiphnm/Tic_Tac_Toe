d5097e05558edef83cafe3088e1c8340cfee4800
Jordandiazfr <jordandiazfr@gmail.com>
2021-01-29 01:35:07 +0100

    all combinations for winners declared

--- assets/game.js
+++ assets/game.js
@@ -7,18 +7,29 @@ let elements = document.querySelectorAll(".element");
 let n = Math.floor(Math.random() * 10);
 let player_1 = false;
 let who_play = "";
+var gameIsOn = true;
+// audio objs
+player_x = new Audio("./assets/audio/xplayer.mp3");
+player_o = new Audio("./assets/audio/oplayer.mp3");
 
 for (const element of elements) {
   element.addEventListener("click", (e) => {
     let value = element.querySelector("span");
-    if (!player_1) {
-      value.innerText = "X";
-      player_1 = true;
-    } else {
-      value.innerText = "O";
-      player_1 = false;
+    if (value.innerText == "") {
+      if (!player_1) {
+        value.classList.add("animation");
+        value.innerText = "X";
+        player_x.play();
+        player_1 = true;
+        is_winner();
+      } else {
+        value.classList.add("animation");
+        value.innerText = "O";
+        player_1 = false;
+        player_o.play();
+        is_winner();
+      }
     }
-    is_winner();
   });
 }
 
@@ -27,18 +38,44 @@ function cell(n) {
 }
 
 function is_winner() {
-  let players = ["X", "O"];
-  players.forEach((player) => {
-    if (cell(0) == player && cell(1) == player && cell(2) == player) {
-      alert("You won !");
-    } else if (cell(4) == player && cell(5) == player && cell(6) == player) {
-      alert("You won !");
-    } else {
-      console.log(elements[0].querySelector("span"));
+  let winning_combinations = [
+    [cell(0), cell(1), cell(2)],
+    [cell(3), cell(4), cell(5)],
+    [cell(6), cell(7), cell(8)],
+    [cell(0), cell(3), cell(6)],
+    [cell(1), cell(4), cell(7)],
+    [cell(2), cell(5), cell(8)],
+    [cell(0), cell(4), cell(8)],
+    [cell(6), cell(4), cell(2)],
+  ];
+  let player_x = ["X", "X", "X"];
+  let player_o = ["O", "O", "O"];
+  winning_combinations.forEach((combination) => {
+    if (arraysEqual(combination, player_x)) {
+      print_winner();
+    }
+    if (arraysEqual(combination, player_o)) {
+      alert("win");
     }
   });
 }
 
+function arraysEqual(a, b) {
+  if (JSON.stringify(a) == JSON.stringify(b)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function print_winner() {
+  elements.forEach((element) => {
+    let player = element.querySelector("span");
+    if (player.innerText == "X") {
+      player.classList.add("rouge");
+    }
+  });
+}
 /* function computer_turn() {
   let n = Math.floor(Math.random() * 10);
   console.log(elements[n]);
